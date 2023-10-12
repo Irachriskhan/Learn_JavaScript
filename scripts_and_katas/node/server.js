ffd399ace01ccf4b7c86aefdefeae7f318924a11
Christophe Irakoze <irachriskhan@gmail.com>
2023-10-12 11:38:40 +0200

    add promises and server initialization

--- scripts_and_katas/node/server.js
+++ scripts_and_katas/node/server.js
@@ -1,3 +1,26 @@
 const http = require("http");
 
-http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("Welcome to our homepage");
+  }
+  if (req.url == "/about") {
+    res.end("Here is our about page");
+  }
+  //   res.end(
+  //     `<h1>Ooops</h1>
+  //     <p>We cannot find the link yu specified</p>`
+  //   );
+});
+server.listen(8080);
+
+// NPM
+// local dependancies - use only in this particular project
+// npm install pkgname
+
+// global dependancies - used in all projects
+// npm install --global pkgname
+
+// package json - manifest filr (store important info about proj/pkg)
+// it is installed manually in the root folder of te project
+// npm init -y (everything default)
--- scripts_and_katas/promise.js
+++ scripts_and_katas/promise.js
@@ -2,11 +2,34 @@
 function getWeather() {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      reject("Error");
-    });
+      resolve("rainny");
+    }, 100);
   });
 }
 
+function getWeatherIcon(weather) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (weather == "sunny") {
+        resolve("☀️☀️☀️☀️☀️☀️");
+      } else if (weather == "cloudy") {
+        resolve("⛅⛅⛅⛅⛅");
+      } else if (weather == "rainny") {
+        resolve("☂️☂️☂️☂️☂️");
+      } else {
+        reject("No Icon found!!");
+      }
+    }, 100);
+  });
+}
+
+function onSuccess(data) {
+  console.log(`The weather is ${data}`);
+}
+
+function onError(data) {
+  console.log(`Error: ${data}`);
+}
+
 // Promise Receiver
-let promise = getWeather();
-console.log(promise);
+getWeather().then(getWeatherIcon).then(onSuccess, onError);
