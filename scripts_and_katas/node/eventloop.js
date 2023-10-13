30a8a3bb6473347e99366f7b91d5d7256652a680
Christophe Irakoze <irachriskhan@gmail.com>
2023-10-13 10:52:53 +0200

    add some exercises from Codeninjas

--- scripts_and_katas/node/eventloop.js
+++ scripts_and_katas/node/eventloop.js
@@ -1,12 +1,53 @@
+let txt = "Dermatoglyphics";
+
+function isIsogram(str) {
+  const splittedTxt = str.toLowerCase().split("");
+  let sum = 0;
+
+  for (element of splittedTxt) {
+    for (letter of splittedTxt) {
+      if (letter === element) {
+        sum++;
+        if (sum === 2) return false;
+      }
+    }
+    sum = 0;
+  }
+  return true;
+}
+
+console.log(isIsogram(txt));
+
+// function isIsogram(str) {
+//   return !str || str.length === new Set(str.toLowerCase()).size;
+// }
+
+// function isIsogram(str) {
+//   return !str.match(/([a-z]).*\1/i);
+// }
+
+// function isIsogram(str) {
+//   return new Set(str.toUpperCase()).size == str.length;
+// }
+
+// function isIsogram(str) {
+//   return !/(\w).*\1/i.test(str);
+// }
+
+// function isIsogram(str) {
+//   var i, j;
+//   str = str.toLowerCase();
+//   for (i = 0; i < str.length; ++i)
+//     for (j = i + 1; j < str.length; ++j) if (str[i] === str[j]) return false;
+//   return true;
+// }
+
 // Async patterns are callback, promises, async-await
 
-// const txt = "abcefjfvdmnj";
-
-// const splittedTxt = txt.split("");
-
 // for (element of splittedTxt) {
-//   for (let i = 0; i < splittedTxt.length; i++) {
-//     console.log(splittedTxt[i]);
+//   for (letter of splittedTxt) {
+//     sum++;
+//     if (letter === element && sum === 2) return letter;
 //   }
 // }
 
@@ -65,13 +106,42 @@ const start = async () => {
     const reading = await readFilePromise("./sample.txt", "utf-8");
     console.log(reading);
 
-    const writing = await updateFilePromise(
-      "./sample.txt",
-      "I like Javascript"
-    );
+    const writing = await updateFilePromise("./sample.txt", "I__");
     console.log(writing);
   } catch (error) {
     console.log(error);
   }
 };
 start();
+
+function getSum(a, b) {
+  let y = 0;
+  if (a == b) return a;
+  else if (a <= b) for (let i = a; i <= b; i++) y += i;
+  else for (let i = a; i >= b; i--) y += i;
+  return y;
+}
+
+getSum(2, 2);
+
+// function GetSum(a, b) {
+//   return ((Math.abs(a - b) + 1) * (a + b)) / 2;
+// }
+
+// const GetSum = (a, b) => {
+//   let min = Math.min(a, b),
+//     max = Math.max(a, b);
+//   return ((max - min + 1) * (min + max)) / 2;
+// };
+
+// function GetSum(a, b) {
+//   if (a == b) return a;
+//   else if (a < b) return a + GetSum(a + 1, b);
+//   else return a + GetSum(a - 1, b);
+// }
+
+// function findNextSquare(sq) {
+//   let x = Math.sqrt(sq);
+//   if (x > Math.floor(x) || x < 0) return -1;
+//   else return (x + 1) ** 2;
+// }
