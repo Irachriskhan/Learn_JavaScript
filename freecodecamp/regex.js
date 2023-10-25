30a0cbc88496d0bd29eb8311e3b2ba79a076a8a0
Christophe Irakoze <irachriskhan@gmail.com>
2023-10-25 16:10:26 +0200

    more regex

--- freecodecamp/regex.js
+++ freecodecamp/regex.js
@@ -89,5 +89,90 @@ bogStr.match(bgRegex);
 
 let quoteSample =
   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[a,e,i,o,u]/gi; // Change this line
+let vowelRegex = /[aeiou]/gi;
 let solution = quoteSample.match(vowelRegex); // Change this line
+
+// Match Letters of the Alphabet
+// You saw how you can use character sets to specify a group of characters to match, but that's a lot of
+// typing when you need to match a large range of characters (for example, every letter in the alphabet).
+// Fortunately, there is a built-in feature that makes this short and simple.
+
+// Match all the letters in the string quoteSample. Note: Be sure to match both uppercase and lowercase letters.
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let solutio = quoteSample.match(alphabetRegex);
+
+// Match Numbers and Letters of the Alphabet
+// Using the hyphen (-) to match a range of characters is not limited to letters. It also
+// works to match a range of numbers.
+// For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+let jennyStr = "Jenny8675309";
+let newRegex = /[a-z0-9]/gi;
+jennyStr.match(newRegex);
+
+// Match Single Characters Not Specified
+// You could also create a set of characters that you do not want to match. These types of character
+// sets are called negated character sets.
+// To create a negated character set, you place a caret character (^) after the opening bracket and before
+// the characters you do not want to match.
+// For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and
+// white space are matched - the negated vowel character set only excludes the vowel characters.
+
+// Create a single regex that matches all characters that are not a number or a vowel. Remember to
+// include the appropriate flags in the regex.
+let quoteSample1 = "3 blind mice.";
+let myRegex1 = /[^aiuoe0-9]/gi;
+let result1 = quoteSample1.match(myRegex1);
+
+// Match Characters that Occur One or More Times
+// you need to match a character (or group of characters) that appears one or more times in a row. This means it
+// occurs at least once, and may be repeated.
+// You can use the + character to check if that is the case.
+// For example, /a+/g would find one match in abc and return ["a"].
+// Because of the +, it would also find a single match in aabc and return ["aa"].
+// If it were instead checking the string abab, it would find two matches and
+// return ["a", "a"] because the a characters are not in a row - there is a b between them.
+
+let difficultSpelling = "Mississippi";
+let myRegex2 = /s+/gi;
+let result2 = difficultSpelling.match(myRegex2);
+
+// Match Characters that Occur Zero or More Times
+
+// The last challenge used the plus + sign to look for characters that occur one or more times. There's also an option that matches
+//  characters that occur zero or more times.
+// The character to do this is the asterisk or star: *.
+
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex);
+gPhrase.match(goRegex);
+oPhrase.match(goRegex);
+
+// For this challenge, chewieQuote has been initialized as the string Aaaaaaaaaaaaaaaarrrgh! behind the scenes.
+// Create a regex chewieRegex that uses the * character to match an uppercase A character immediately followed
+// by zero or more lowercase a characters in chewieQuote.
+// Your regex does not need flags or character classes, and it should not match any of the other quotes.
+
+const chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/;
+let result3 = chewieQuote.match(chewieRegex);
+
+// Find Characters with Lazy Matching
+
+// In regular expressions, a greedy match finds the longest possible part of a string that fits the regex
+// pattern and returns it as a match. The alternative is called a lazy match, which finds the smallest
+// possible part of the string that satisfies the regex pattern.
+
+// You can apply the regex /t[a-z]*i/ to the string "titanic". This regex is basically a pattern that starts
+// with t, ends with i, and has some letters in between.
+// Regular expressions are by default greedy, so the match would return ["titani"]. It finds the largest
+// sub-string possible to fit the pattern.
+// However, you can use the ? character to change it to lazy matching. "titanic" matched against the adjusted
+// regex of /t[a-z]*?i/ returns ["ti"].
+// Note: Parsing HTML with regular expressions should be avoided, but pattern matching an HTML string with regular
+// expressions is completely fine.
+
+// Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter is coming</h1>". Remember the wildcard . in a regular expression matches any character.
