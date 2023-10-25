6166c574fca17cf80ee8c8b8166ac2a83e805806
Christophe Irakoze <irachriskhan@gmail.com>
2023-10-25 16:10:01 +0200

    add, delete and update in express

--- expressJS/app.js
+++ expressJS/app.js
@@ -3,6 +3,7 @@ const app = express();
 const loggers = require("./logger");
 const authorize = require("./authorize");
 const morgan = require("morgan");
+let { people } = require("./data");
 
 // const port = 3000;
 
@@ -85,26 +86,120 @@ const morgan = require("morgan");
 
 // Three types of modules: own, express, third-parties
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  return res.send("Homepage");
+// app.get("/home", (req, res) => {
+//   console.log(req.query);
+//   return res.send("Homepage");
+// });
+
+// // app.use(loggers.logger); // When positionned here, the homepage cannot access it
+
+// app.get("/about", (req, res) => {
+//   res.send("This is about page");
+// });
+
+// app.get("/api/products", (req, res) => {
+//   res.send("This is the  list of products");
+// });
+
+// // ============================================================================
+// // ============================================================================
+// // CRUD methods
+
+// let { people } = require("./data");
+
+// app.get("/api/people", (req, res) => {
+//   console.log("People");
+//   // res.status(200).json(people); // access the data file in form of json
+//   res.status(200).json({ success: true, data: people });
+// });
+
+// // static assets
+// app.use(express.static("./methods-public")); // call the whole folder without using get method
+
+// app.post("/api/people", (req, res) => {
+//   const { name } = req.body;
+//   if (!name) return res.status(400).json({ success: false });
+// });
+
+// =====================================================================================
+// =====================================================================================
+
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-// app.use(loggers.logger); // When positionned here, the homepage cannot access it
-
-app.get("/about", (req, res) => {
-  res.send("This is about page");
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
-app.get("/api/products", (req, res) => {
-  res.send("This is the  list of products");
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send("Please Provide Credentials");
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
-// ============================================================================
-// ============================================================================
