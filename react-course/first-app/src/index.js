df2e95d088206ecd4c20227e2d17dcbbc8d21df7
Christophe Irakoze <irachriskhan@gmail.com>
2023-12-13 12:39:18 +0200

    reset the root files

--- react-course/first-app/src/index.js
+++ react-course/first-app/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// CSS
+import "./index.css"; // import css file
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Setup vars
+const title = "Presentation of VoY Project";
+const authFirstName = "Christophe";
+const authLastName = "Irakoze";
+const img = "../images/image2.png";
+
+// Nested Components
+function Booklist() {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <img src={img} alt="Image" />
+      <h1>{title}</h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.95rem", marginTop: "0.25rem" }}
+      >
+        {authFirstName} {authLastName.toUpperCase()}
+      </h4>
+      {/* <p>{let x = 5}</p> */}
+      <p>Downloads: {6 + 6}</p>
+    </article>
+  );
+};
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
