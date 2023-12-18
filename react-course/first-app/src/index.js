3f2887782cd6c978f0193150063d93c2eff60297
Christophe Irakoze <irachriskhan@gmail.com>
2023-12-18 15:37:46 +0200

    props and spread operator

--- react-course/first-app/src/index.js
+++ react-course/first-app/src/index.js
@@ -7,43 +7,59 @@ import "./index.css"; // import css file
 // Setup vars
 const books = [
   {
+    id: 1,
     title: "Presentation of VoY Project",
     img: "../images/image2.png",
     authFirstName: "Christophe",
     authLastName: "Irakoze",
   },
   {
+    id: 2,
     title: "Mahama Livelyhood",
     img: "../images/image1.png",
     authFirstName: "Khan",
     authLastName: "Aganze",
   },
+  {
+    id: 3,
+    title: "Alight CS",
+    img: "../images/image3.png",
+    authFirstName: "John",
+    authLastName: "Peter",
+  },
 ];
 
-const names = ["John", "Paul", "Khan", "Markus", "Hansen"];
-const newNames = names.map((name) => {
-  return <h3>{name}</h3>;
-});
+// const names = ["John", "Paul", "Khan", "Markus", "Hansen"];
+// const newNames = names.map((name) => {
+//   return <h3>{name}</h3>;
+// });
 // Nested Components
 // we place the arguments where we render the function, not where is declared
 function Booklist() {
-  return <section className="booklist">{newNames}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
 
 {
   /* props parameter is an object */
 }
-const Book = ({
-  img,
-  title,
-  authFirstName,
-  authLastName,
-  coAuthor,
-  number,
-  children,
-}) => {
+
+const Book = (props) => {
   // console.log(props);
-  // const { img, title, authFirstName, authLastName, coAuthor,number } = props;
+  const {
+    img,
+    title,
+    authFirstName,
+    authLastName,
+    coAuthor,
+    number,
+    children,
+  } = props;
   return (
     <article className="book">
       <img src={img} alt="Image" />
@@ -60,7 +76,6 @@ const Book = ({
           {authFirstName} {authLastName}{" "}
         </span>
       </h4>
-      {children}
 
       <p>{coAuthor}</p>
       {/* <p>{let x = 5}</p> */}
@@ -71,3 +86,4 @@ const Book = ({
 
 ReactDom.render(<Booklist />, document.getElementById("root"));
 // createRoot.render(<Booklist />, document.getElementById("root"));
+// 2:44:55
